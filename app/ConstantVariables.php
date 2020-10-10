03123a8a4361fb5727874b011d9dd5cacf6a5b7d
daavka1127 <davaa4510@gmail.com>
2020-10-10 20:00:57 +0800

    constant class zasav

--- app/ConstantVariables.php
+++ app/ConstantVariables.php
@@ -8,7 +8,7 @@ class ConstantVariables
     const sheepKG = 18.7;
 
     const greenDay = 60; /*60 aas deeshee*/
-    const yellow = 60 /*60 aas doosh bol shar*/
+    const yellow = 60; /*60 aas doosh bol shar*/
     const orange = 29; /*29 uus doosh bol ulbar shar*/
     const redDay = 15; /*15 aas doosh bol ulaan*/
 }
