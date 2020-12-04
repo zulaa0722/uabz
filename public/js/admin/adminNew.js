b5b251d00e0fddb8a68d5466187cb154d47921fa
Zulaa <zulaa.0722@gmail.com>
2020-12-04 10:49:49 -0700

    test

--- public/js/admin/adminNew.js
+++ public/js/admin/adminNew.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 
 function IsEmail(email) {
   var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if(!regex.test(email)) {
+  if( !regex.test(email) ) {
     return false;
   }else{
     return true;
